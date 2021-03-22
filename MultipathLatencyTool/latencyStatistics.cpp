c2d90c8607d203d60e96999c0d4013c210abac13
Guillaume Hetier <guhetier@microsoft.com>
2021-03-22 11:19:12 -0700

    Refactor statistics computation using views

--- MultipathLatencyTool/latencyStatistics.cpp
+++ MultipathLatencyTool/latencyStatistics.cpp
@@ -1,7 +1,11 @@
 ﻿#include "latencyStatistics.h"
 
+#include <algorithm>
+#include <functional>
 #include <iostream>
 #include <limits>
+#include <numeric>
+#include <ranges>
 #include <vector>
 
 namespace multipath {
@@ -11,90 +15,110 @@ constexpr long long ConvertHundredNanosToMillis(long long hundredNs) noexcept
     return static_cast<long long>(hundredNs / 10000LL);
 }
 
+template <std::ranges::range R, class T>
+[[nodiscard]] constexpr T accumulate(R&& range, T val)
+{
+    return std::accumulate(std::begin(range), std::end(range), val);
+}
+
+template <std::ranges::range R>
+auto to_vector(R&& r, size_t sizeHint = 0)
+{
+    std::vector<std::ranges::range_value_t<R>> v;
+
+    // if we can get a size, reserve that much
+    if (sizeHint != 0)
+    {
+        v.reserve(sizeHint);
+    }
+
+    // push all the elements
+    for (auto&& e : r)
+    {
+        v.push_back(static_cast<decltype(e)&&>(e));
+    }
+
+    return v;
+}
+
 void PrintLatencyStatistics(std::span<const LatencyData> data)
 {
-    // simple average of latencies for received datagrams
-    long long primaryLatencyTotal = 0;
-    long long secondaryLatencyTotal = 0;
-    long long aggregatedLatencyTotal = 0;
-    long long effectiveLatencyTotal = 0;
+    using namespace std::views;
 
-    long long primaryReceivedFrames = 0;
-    long long secondaryReceivedFrames = 0;
-    long long aggregatedLatencySamples = 0;
-    long long effectiveLatencySamples = 0;
-
-    long long primaryLostFrames = 0;
-    long long secondaryLostFrames = 0;
-    long long aggregatedLostFrames = 0;
-
-    long long primarySentFrames = 0;
-    long long secondarySentFrames = 0;
-
-    for (const auto& stat : data)
-    {
-        long long aggregatedLatency = std::numeric_limits<long long>::max();
-        if (stat.m_primarySendTimestamp >= 0)
+    // Lambdas for selecting, filtering and transforming data
+    auto selectPrimary = [](const LatencyData& stat) {
+        return std::make_pair(stat.m_primarySendTimestamp, stat.m_primaryReceiveTimestamp);
+    };
+    auto selectSecondary = [](const LatencyData& stat) {
+        return std::make_pair(stat.m_secondarySendTimestamp, stat.m_secondaryReceiveTimestamp);
+    };
+    auto selectEffective = [](const LatencyData& stat) {
+        auto effectiveSend = -1LL;
+        if (stat.m_primarySendTimestamp >= 0 && stat.m_secondarySendTimestamp >= 0)
         {
-            primarySentFrames += 1;
-        }
-
-        if (stat.m_primaryReceiveTimestamp >= 0)
-        {
-            const auto primaryLatency = stat.m_primaryReceiveTimestamp - stat.m_primarySendTimestamp;
-            primaryLatencyTotal += primaryLatency;
-            primaryReceivedFrames += 1;
-            aggregatedLatency = primaryLatency;
-        }
-        else if (stat.m_primarySendTimestamp >= 0)
-        {
-            primaryLostFrames += 1;
-        }
-
-        if (stat.m_secondarySendTimestamp >= 0)
-        {
-            secondarySentFrames += 1;
-        }
-
-        if (stat.m_secondaryReceiveTimestamp >= 0)
-        {
-            const auto secondaryLatency = stat.m_secondaryReceiveTimestamp - stat.m_secondarySendTimestamp;
-            secondaryLatencyTotal += secondaryLatency;
-            secondaryReceivedFrames += 1;
-            aggregatedLatency = std::min(aggregatedLatency, secondaryLatency);
-        }
-        else if (stat.m_secondarySendTimestamp >= 0)
-        {
-            secondaryLostFrames += 1;
-        }
-
-        if (stat.m_secondaryReceiveTimestamp >= 0 || stat.m_primaryReceiveTimestamp >= 0)
-        {
-            aggregatedLatencyTotal += aggregatedLatency;
-            aggregatedLatencySamples += 1;
+            effectiveSend = std::min(stat.m_primarySendTimestamp, stat.m_secondarySendTimestamp);
         }
         else
         {
-            aggregatedLostFrames += 1;
+            effectiveSend = std::max(stat.m_primarySendTimestamp, stat.m_secondarySendTimestamp);
         }
 
-        if (stat.m_secondaryReceiveTimestamp >= 0 && stat.m_primaryReceiveTimestamp >= 0)
+        auto effectiveReceive = -1LL;
+        if (stat.m_primaryReceiveTimestamp >= 0 && stat.m_secondaryReceiveTimestamp >= 0)
         {
-            effectiveLatencyTotal += std::min(stat.m_secondaryReceiveTimestamp, stat.m_primaryReceiveTimestamp) -
-                                     std::min(stat.m_secondarySendTimestamp, stat.m_primarySendTimestamp);
-            effectiveLatencySamples += 1;
+            effectiveReceive = std::min(stat.m_primaryReceiveTimestamp, stat.m_secondaryReceiveTimestamp);
         }
-    }
+        else
+        {
+            effectiveReceive = std::max(stat.m_primaryReceiveTimestamp, stat.m_secondaryReceiveTimestamp);
+        }
+        return std::make_pair(effectiveSend, effectiveReceive);
+    };
 
-    const auto primaryAverageLatency =
-        ConvertHundredNanosToMillis(primaryReceivedFrames > 0 ? primaryLatencyTotal / primaryReceivedFrames : 0);
-    const auto secondaryAverageLatency =
-        ConvertHundredNanosToMillis(secondaryReceivedFrames > 0 ? secondaryLatencyTotal / secondaryReceivedFrames : 0);
-    const auto aggregatedAverageLatency =
-        ConvertHundredNanosToMillis(aggregatedLatencySamples > 0 ? aggregatedLatencyTotal / aggregatedLatencySamples : 0);
-    const auto effectiveAverageLatency =
-        ConvertHundredNanosToMillis(effectiveLatencySamples > 0 ? effectiveLatencyTotal / effectiveLatencySamples : 0);
-    const auto aggregatedSendFrames = std::max(primarySentFrames, secondarySentFrames);
+    auto received = [](const auto& timestamps) { return timestamps.second >= 0; };
+    auto receivedOnOneInterface = [](const LatencyData& stat) {
+        return stat.m_primaryReceiveTimestamp >= 0 || stat.m_secondaryReceiveTimestamp >= 0;
+    };
+
+    auto latency = [](const auto& timestamps) { return timestamps.second - timestamps.first; };
+    auto minimumLatency = [](const LatencyData& stat) {
+        auto minLatency = std::numeric_limits<long long>::max();
+        if (stat.m_primaryReceiveTimestamp >= 0)
+        {
+            minLatency = std::min(minLatency, stat.m_primaryReceiveTimestamp - stat.m_primarySendTimestamp);
+        }
+        if (stat.m_secondaryReceiveTimestamp >= 0)
+        {
+            minLatency = std::min(minLatency, stat.m_secondaryReceiveTimestamp - stat.m_secondarySendTimestamp);
+        }
+        return minLatency;
+    };
+
+    auto average = [](auto& data) { return data.size() > 0 ? accumulate(data, 0LL) / data.size() : 0LL; };
+    auto percent = [](auto a, auto b) { return b > 0 ? a * 100 / b : 0; };
+
+    // Compute latencies on primary, secondary or both simultaneously
+    auto primaryLatencies = to_vector(data | transform(selectPrimary) | filter(received) | transform(latency), data.size());
+    auto secondaryLatencies = to_vector(data | transform(selectSecondary) | filter(received) | transform(latency), data.size());
+    auto minimumLatencies = to_vector(data | filter(receivedOnOneInterface) | transform(minimumLatency), data.size());
+    auto effectiveLatencies = to_vector(data | transform(selectEffective) | filter(received) | transform(latency), data.size());
+
+    long long primaryReceivedFrames = primaryLatencies.size();
+    long long secondaryReceivedFrames = secondaryLatencies.size();
+    long long aggregatedReceivedFrames = minimumLatencies.size();
+
+    long long primarySentFrames =
+        std::ranges::count_if(data, [](const auto& stat) { return stat.m_primarySendTimestamp >= 0; });
+    long long secondarySentFrames =
+        std::ranges::count_if(data, [](const auto& stat) { return stat.m_secondarySendTimestamp >= 0; });
+    long long aggregatedSentFrames = std::ranges::count_if(
+        data, [](const auto& stat) { return stat.m_primarySendTimestamp >= 0 || stat.m_secondarySendTimestamp >= 0; });
+
+    // Get the average latency
+    const auto primaryAverageLatency = ConvertHundredNanosToMillis(average(primaryLatencies));
+    const auto secondaryAverageLatency = ConvertHundredNanosToMillis(average(secondaryLatencies));
+    const auto aggregatedAverageLatency = ConvertHundredNanosToMillis(average(minimumLatencies));
+    const auto effectiveAverageLatency = ConvertHundredNanosToMillis(average(effectiveLatencies));
 
     std::cout << '\n';
     std::cout << "Sent frames on primary interface: " << primarySentFrames << '\n';
@@ -102,27 +126,31 @@ void PrintLatencyStatistics(std::span<const LatencyData> data)
 
     std::cout << '\n';
     std::cout << "Received frames on primary interface: " << primaryReceivedFrames << " ("
-              << (primarySentFrames > 0 ? primaryReceivedFrames * 100 / primarySentFrames : 0) << "%)\n";
+              << percent(primaryReceivedFrames, primarySentFrames) << "%)\n";
     std::cout << "Received frames on secondary interface: " << secondaryReceivedFrames << " ("
-              << (secondarySentFrames > 0 ? secondaryReceivedFrames * 100 / secondarySentFrames : 0) << "%)\n";
+              << percent(secondaryReceivedFrames, secondarySentFrames) << "%)\n";
 
     std::cout << '\n';
     std::cout << "Average latency on primary interface: " << primaryAverageLatency << '\n';
     std::cout << "Average latency on secondary interface: " << secondaryAverageLatency << '\n';
-    std::cout << "Average minimum latency on combined interface: " << aggregatedAverageLatency << " ("
-              << (primaryAverageLatency > 0 ? (primaryAverageLatency - aggregatedAverageLatency) * 100 / primaryAverageLatency : 0)
-              << "% improvement over primary) \n";
+    std::cout << "Average minimum latency on both interface: " << aggregatedAverageLatency << " ("
+              << percent(primaryAverageLatency - aggregatedAverageLatency, primaryAverageLatency)
+              << "% difference with primary) \n";
     std::cout << "Average effective latency on combined interface: " << effectiveAverageLatency << " ("
-              << (primaryAverageLatency > 0 ? (primaryAverageLatency - effectiveAverageLatency) * 100 / primaryAverageLatency : 0)
-              << "% improvement over primary) \n";
+              << percent(primaryAverageLatency - effectiveAverageLatency, primaryAverageLatency)
+              << "% difference with primary) \n";
+
+    long long primaryLostFrames = primarySentFrames - primaryReceivedFrames;
+    long long secondaryLostFrames = secondarySentFrames - secondaryReceivedFrames;
+    long long aggregatedLostFrames = aggregatedSentFrames - aggregatedReceivedFrames;
 
     std::cout << '\n';
     std::cout << "Lost frames on primary interface: " << primaryLostFrames << " ("
-              << (primarySentFrames > 0 ? (primaryLostFrames * 100 / primarySentFrames) : 0) << "%)\n";
+              << percent(primaryLostFrames, primarySentFrames) << "%)\n";
     std::cout << "Lost frames on secondary interface: " << secondaryLostFrames << " ("
-              << (secondarySentFrames > 0 ? (secondaryLostFrames * 100 / secondarySentFrames) : 0) << "%)\n";
+              << percent(secondaryLostFrames, secondarySentFrames) << "%)\n";
     std::cout << "Lost frames on both interface simultaneously: " << aggregatedLostFrames << " ("
-              << (aggregatedSendFrames > 0 ? (aggregatedLostFrames * 100 / aggregatedSendFrames) : 0) << "%)\n";
+              << percent(aggregatedLostFrames, aggregatedSentFrames) << "%)\n";
 }
 
 void DumpLatencyData(std::span<const LatencyData> data, std::ofstream& file)
