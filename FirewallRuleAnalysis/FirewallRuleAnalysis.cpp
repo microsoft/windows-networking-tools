5620e9a17cf076d6045de8a40446d1e358f7b985
Keith Horton <khorton@microsoft.com>
2024-09-20 15:04:12 -0700

    Initial implementation looking through all Admin rules for duplicates

---
+++ FirewallRuleAnalysis/FirewallRuleAnalysis.cpp
@@ -0,0 +1,103 @@
+/*
+ * FwRuleAnalysis
+ *
+ * - Provides users the ability to find rundancies and inconsistencies in the Windows Firewall rules
+ *
+ */
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+#include <Windows.h>
+#include <netfw.h>
+
+#include <wil/com.h>
+#include <wil/resource.h>
+
+#include "FirewallRuleBuilder.h"
+
+inline std::vector<NormalizedRuleInfo> BuildFirewallRules(_In_ INetFwRules* rules)
+{
+	std::vector<NormalizedRuleInfo> returnInfo;
+
+    long count{};
+    THROW_IF_FAILED(rules->get_Count(&count));
+
+    // enumerators loop until S_FALSE
+    wil::com_ptr<IEnumVARIANT> enumVariant;
+    THROW_IF_FAILED(rules->get__NewEnum(enumVariant.put_unknown()));
+    HRESULT nextResult{S_OK};
+    while (nextResult == S_OK)
+    {
+        wil::unique_variant nextInstance;
+        ULONG fetched{};
+        nextResult = enumVariant->Next(1, nextInstance.addressof(), &fetched);
+        THROW_IF_FAILED(nextResult);
+        if (nextResult == S_OK)
+        {
+            wil::com_ptr<INetFwRule> nextRule;
+            THROW_IF_FAILED(nextInstance.punkVal->QueryInterface<INetFwRule>(&nextRule));
+            returnInfo.emplace_back(BuildFirewallRuleInfo(nextRule.get()));
+        }
+    }
+
+    return returnInfo;
+}
+
+int main()
+try
+{
+    const auto uninit = wil::CoInitializeEx();
+
+	const auto firewallPolicy = wil::CoCreateInstance<NetFwPolicy2, INetFwPolicy2>();
+
+	wil::com_ptr<INetFwRules> rules{};
+	THROW_IF_FAILED(firewallPolicy->get_Rules(&rules));
+	std::vector<NormalizedRuleInfo> normalizedRules = BuildFirewallRules(rules.get());
+
+	wil::com_ptr<INetFwServiceRestriction> serviceRestriction{};
+	THROW_IF_FAILED(firewallPolicy->get_ServiceRestriction(&serviceRestriction));
+	THROW_IF_FAILED(serviceRestriction->get_Rules(&rules));
+	std::vector<NormalizedRuleInfo> normalizedServiceRules = BuildFirewallRules(rules.get());
+
+    // now sort and find duplicates
+	std::sort(normalizedRules.begin(), normalizedRules.end());
+    std::sort(normalizedServiceRules.begin(), normalizedServiceRules.end());
+
+    // find duplicates
+	uint32_t totalDuplicateCount{};
+	std::vector<NormalizedRuleInfo>::iterator startingIterator = normalizedRules.begin();
+    for (;;)
+    {
+		auto duplicateRuleIterator = std::adjacent_find(startingIterator, normalizedRules.end());
+		if (duplicateRuleIterator == normalizedRules.cend())
+		{
+			break;
+		}
+        ++totalDuplicateCount;
+
+		// find all duplicates of this instance
+        auto localDuplicateIterator = duplicateRuleIterator;
+        uint32_t localDuplicateRuleCount{};
+        for (;;)
+        {
+	        if (*localDuplicateIterator != *(localDuplicateIterator + 1))
+	        {
+				startingIterator = localDuplicateIterator + 1;
+                break;
+			}
+            ++localDuplicateRuleCount;
+			++localDuplicateIterator;
+        }
+		wprintf(L"\nDuplicate rule found! Duplicate count (%u)\n", localDuplicateRuleCount);
+        // startingIterator is now updated to the next rule after the duplicates
+		while (duplicateRuleIterator != startingIterator)
+		{
+			wprintf(L"\tname: %ws, description: %ws\n", duplicateRuleIterator->ruleName.get(), duplicateRuleIterator->ruleDescription.get());
+            ++duplicateRuleIterator;
+        }
+    }
+
+	wprintf(L"\n\nTotal rules: %llu\nTotal duplicate rules: %u\n", normalizedRules.size(), totalDuplicateCount);
+}
+CATCH_RETURN()
---
+++ FirewallRuleAnalysis/FirewallRuleBuilder.h
@@ -0,0 +1,242 @@
+#pragma once
+#include <string>
+
+#include <Windows.h>
+#include <netfw.h>
+
+#include <wil/com.h>
+#include <wil/resource.h>
+
+struct NormalizedRuleInfo
+{
+	wil::unique_bstr ruleName;
+	wil::unique_bstr ruleDescription;
+	std::wstring normalizedRuleDetails;
+
+	// guarantee this object is never copied, only moved
+	NormalizedRuleInfo(const NormalizedRuleInfo&) = delete;
+	NormalizedRuleInfo& operator=(const NormalizedRuleInfo&) = delete;
+
+	NormalizedRuleInfo(NormalizedRuleInfo&&) = default;
+	NormalizedRuleInfo& operator=(NormalizedRuleInfo&&) = default;
+
+	NormalizedRuleInfo() = default;
+	~NormalizedRuleInfo() = default;
+
+	void AppendValue(const wil::unique_bstr& value)
+	{
+		if (value && value.get()[0] != L'\0')
+		{
+			normalizedRuleDetails.append(value.get());
+		}
+	}
+
+	// the one VARIANT that's returned from the Firewall Rule interface
+	// is for an array of strings for Interfaces
+	void AppendValue(const wil::unique_variant& value)
+	{
+		if (value.vt == VT_EMPTY)
+		{
+			// it's acceptable to be either EMPTY
+			// or an ARRAY of VARIANTs
+			return;
+		}
+		if (value.vt != (VT_ARRAY | VT_VARIANT))
+		{
+			THROW_HR(E_UNEXPECTED);
+		}
+
+		const auto arrayDimensions = SafeArrayGetDim(value.parray);
+		for (UINT i = 0; i < arrayDimensions; ++i)
+		{
+			LONG lBound{}; // lower bound
+			THROW_IF_FAILED(SafeArrayGetLBound(value.parray, i + 1, &lBound));
+			LONG uBound{}; // upper bound
+			THROW_IF_FAILED(SafeArrayGetUBound(value.parray, i + 1, &uBound));
+
+			for (LONG j = lBound; j <= uBound; ++j)
+			{
+				wil::unique_variant element{};
+				THROW_IF_FAILED(SafeArrayGetElement(value.parray, &j, &element));
+
+				if (element.vt != VT_BSTR)
+				{
+					THROW_HR(E_UNEXPECTED);
+				}
+
+				if (element.bstrVal && element.bstrVal[0] != L'\0')
+				{
+					normalizedRuleDetails.append(element.bstrVal);
+				}
+			}
+		}
+	}
+
+	template <typename T>
+	void AppendValue(T t)
+	{
+		// ensure conversion from the type provided by the caller
+		const int32_t convertedValue{ t };
+		if (convertedValue != 0)
+		{
+			normalizedRuleDetails.append(std::to_wstring(convertedValue));
+		}
+	}
+};
+
+inline bool operator==(const NormalizedRuleInfo& lhs, const NormalizedRuleInfo& rhs) noexcept
+{
+	if (lhs.normalizedRuleDetails.size() != rhs.normalizedRuleDetails.size())
+	{
+		return false;
+	}
+
+	constexpr BOOL bIgnoreCase = TRUE;
+	return CSTR_EQUAL == CompareStringOrdinal(
+		lhs.normalizedRuleDetails.c_str(),
+		static_cast<int>(lhs.normalizedRuleDetails.size()),
+		rhs.normalizedRuleDetails.c_str(),
+		static_cast<int>(rhs.normalizedRuleDetails.size()),
+		bIgnoreCase);
+}
+inline bool operator!=(const NormalizedRuleInfo& lhs, const NormalizedRuleInfo& rhs) noexcept
+{
+	return !(lhs == rhs);
+}
+
+inline bool operator<(const NormalizedRuleInfo& lhs, const NormalizedRuleInfo& rhs) noexcept
+{
+	if (lhs.normalizedRuleDetails.size() != rhs.normalizedRuleDetails.size())
+	{
+		return lhs.normalizedRuleDetails.size() < rhs.normalizedRuleDetails.size();
+	}
+
+	constexpr BOOL bIgnoreCase = TRUE;
+	return CSTR_LESS_THAN == CompareStringOrdinal(
+		lhs.normalizedRuleDetails.c_str(),
+		static_cast<int>(lhs.normalizedRuleDetails.size()),
+		rhs.normalizedRuleDetails.c_str(),
+		static_cast<int>(rhs.normalizedRuleDetails.size()),
+		bIgnoreCase);
+}
+inline bool operator>(const NormalizedRuleInfo& lhs, const NormalizedRuleInfo& rhs) noexcept
+{
+	return !(lhs < rhs);
+}
+
+inline NormalizedRuleInfo BuildFirewallRuleInfo(const wil::com_ptr<INetFwRule>& rule)
+{
+	NormalizedRuleInfo ruleInfo{};
+
+	// QI to the latest firewall rule interface
+	wil::com_ptr<INetFwRule3> latestRule;
+	rule.query_to<INetFwRule3>(&latestRule);
+
+	// name and description are volatile - they don't impact the final filter
+	THROW_IF_FAILED(latestRule->get_Name(&ruleInfo.ruleName));
+	THROW_IF_FAILED(latestRule->get_Description(&ruleInfo.ruleDescription));
+
+	try
+	{
+		wil::unique_bstr applicationName{};
+		THROW_IF_FAILED(latestRule->get_ApplicationName(&applicationName));
+		ruleInfo.AppendValue(applicationName);
+
+		wil::unique_bstr serviceName{};
+		THROW_IF_FAILED(latestRule->get_ServiceName(&serviceName));
+		ruleInfo.AppendValue(serviceName);
+
+		LONG protocol{};
+		THROW_IF_FAILED(latestRule->get_Protocol(&protocol));
+		ruleInfo.AppendValue(protocol);
+
+		wil::unique_bstr localPorts{};
+		THROW_IF_FAILED(latestRule->get_LocalPorts(&localPorts));
+		ruleInfo.AppendValue(localPorts);
+
+		wil::unique_bstr remotePorts{};
+		THROW_IF_FAILED(latestRule->get_RemotePorts(&remotePorts));
+		ruleInfo.AppendValue(remotePorts);
+
+		wil::unique_bstr localAddresses{};
+		THROW_IF_FAILED(latestRule->get_LocalAddresses(&localAddresses));
+		ruleInfo.AppendValue(localAddresses);
+
+		wil::unique_bstr remoteAddresses{};
+		THROW_IF_FAILED(latestRule->get_RemoteAddresses(&remoteAddresses));
+		ruleInfo.AppendValue(remoteAddresses);
+
+		wil::unique_bstr icmpTypesAndCodes{};
+		THROW_IF_FAILED(latestRule->get_IcmpTypesAndCodes(&icmpTypesAndCodes));
+		ruleInfo.AppendValue(icmpTypesAndCodes);
+
+		NET_FW_RULE_DIRECTION direction{};
+		THROW_IF_FAILED(latestRule->get_Direction(&direction));
+		ruleInfo.AppendValue(direction);
+
+		wil::unique_variant interfaces;
+		THROW_IF_FAILED(latestRule->get_Interfaces(&interfaces));
+		ruleInfo.AppendValue(interfaces);
+
+		wil::unique_bstr interfaceTypes;
+		THROW_IF_FAILED(latestRule->get_InterfaceTypes(&interfaceTypes));
+		ruleInfo.AppendValue(interfaceTypes);
+
+		// not going to require matching enabled vs disabled when matching the rules
+		// VARIANT_BOOL enabled{};
+		// THROW_IF_FAILED(latestRule->get_Enabled(&enabled));
+		// ruleInfo.AppendValue(enabled);
+
+		wil::unique_bstr grouping{};
+		THROW_IF_FAILED(latestRule->get_Grouping(&grouping));
+		ruleInfo.AppendValue(grouping);
+
+		long profiles{};
+		THROW_IF_FAILED(latestRule->get_Profiles(&profiles));
+		ruleInfo.AppendValue(profiles);
+
+		VARIANT_BOOL edgeTraversal{};
+		THROW_IF_FAILED(latestRule->get_EdgeTraversal(&edgeTraversal));
+		ruleInfo.AppendValue(edgeTraversal);
+
+		NET_FW_ACTION action{};
+		THROW_IF_FAILED(latestRule->get_Action(&action));
+		ruleInfo.AppendValue(action);
+
+		long edgeTraversalOptions{};
+		THROW_IF_FAILED(latestRule->get_EdgeTraversalOptions(&edgeTraversalOptions));
+		ruleInfo.AppendValue(edgeTraversalOptions);
+
+		wil::unique_bstr localAppPackageId{};
+		THROW_IF_FAILED(latestRule->get_LocalAppPackageId(&localAppPackageId));
+		ruleInfo.AppendValue(localAppPackageId);
+
+		wil::unique_bstr localUserOwner{};
+		THROW_IF_FAILED(latestRule->get_LocalUserOwner(&localUserOwner));
+		ruleInfo.AppendValue(localUserOwner);
+
+		wil::unique_bstr localUserAuthorizedList{};
+		THROW_IF_FAILED(latestRule->get_LocalUserAuthorizedList(&localUserAuthorizedList));
+		ruleInfo.AppendValue(localUserAuthorizedList);
+
+		wil::unique_bstr remoteUserAuthorizedList{};
+		THROW_IF_FAILED(latestRule->get_RemoteUserAuthorizedList(&remoteUserAuthorizedList));
+		ruleInfo.AppendValue(remoteUserAuthorizedList);
+
+		wil::unique_bstr remoteMachineAuthorizedList{};
+		THROW_IF_FAILED(latestRule->get_RemoteMachineAuthorizedList(&remoteMachineAuthorizedList));
+		ruleInfo.AppendValue(remoteMachineAuthorizedList);
+
+		long secureFlags{};
+		THROW_IF_FAILED(latestRule->get_SecureFlags(&secureFlags));
+		ruleInfo.AppendValue(secureFlags);
+	}
+	catch (...)
+	{
+		wprintf(L"Failed to read rule %ws (%ws) - 0x%x\n",
+			ruleInfo.ruleName.get(), ruleInfo.ruleDescription.get(), wil::ResultFromCaughtException());
+		throw;
+	}
+
+	return ruleInfo;
+}
